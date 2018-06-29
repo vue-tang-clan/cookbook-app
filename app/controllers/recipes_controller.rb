c70c383902ea05598846d5e222e6b72a03dd1aee
Peter Jang <peter.x.jang@gmail.com>
2018-06-28 19:57:41 -0500

    Add RESTful recipes create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,17 @@ class RecipesController < ApplicationController
     render "index.json.jbuilder"
   end
 
+  def create
+    @recipe = Recipe.new(
+      title: params[:input_title],
+      chef: params[:input_chef],
+      ingredients: params[:input_ingredients],
+      directions: params[:input_directions]
+    )
+    @recipe.save
+    render "show.json.jbuilder"
+  end
+
   def show
     @recipe = Recipe.find_by(id: params[:id])
     render "show.json.jbuilder"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/recipes" => "recipes#index"
+  post "/recipes" => "recipes#create"
   get "/recipes/:id" => "recipes#show"
 end
