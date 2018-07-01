e06123be12e60ec708b86f6425447f609448c6ce
Peter Jang <peter.x.jang@gmail.com>
2018-07-01 10:42:52 -0500

    Add model methods for formatting

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
