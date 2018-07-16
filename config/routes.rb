7c8544a44255a10f729d0d4b305fddc3068154f4
Peter Jang <peter.x.jang@gmail.com>
2018-07-16 18:12:42 -0500

    Use api namespace for all actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get "/recipes" => "recipes#index"
-  post "/recipes" => "recipes#create"
-  get "/recipes/:id" => "recipes#show"
-  patch "/recipes/:id" => "recipes#update"
-  delete "/recipes/:id" => "recipes#destroy"
+  namespace :api do
+    get "/recipes" => "recipes#index"
+    post "/recipes" => "recipes#create"
+    get "/recipes/:id" => "recipes#show"
+    patch "/recipes/:id" => "recipes#update"
+    delete "/recipes/:id" => "recipes#destroy"
 
-  post "/users" => "users#create"
+    post "/users" => "users#create"
 
-  post "/sessions" => "sessions#create"
+    post "/sessions" => "sessions#create"
+  end
 end
