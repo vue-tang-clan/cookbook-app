2e938973307a4a274ba96cd87042ad9e4d02272a
Peter Jang <peter.x.jang@gmail.com>
2018-07-18 18:37:19 -0500

    Add recipes html edit action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -26,4 +26,9 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params[:id])
     render "show.html.erb"
   end
+
+  def edit
+    @recipe = Recipe.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
 end
